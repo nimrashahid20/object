911d16229ed0e5d652b3a3087c626867045dfce2
nimrashahid20 <nimrashahid02007@gmail.com>
2024-08-26 15:46:30 +0500

    intro

---
+++ index.js
@@ -0,0 +1,71 @@
+
+
+// let plan1 = {
+//     name : ["basic","Professional"],
+//     pages : 10,
+//     space: 100,
+//     price: 3.99,
+//     transfer: 1000
+// }
+// plan1.price = 455
+// console.log(plan1.name[1])
+// console.log(plan1)
+// delete plan1.space
+
+// let plan2 = {
+//     name : "Professional",
+//     price:  5.99,
+//     space:  50,
+//     transfer : 2000,
+//     pages: 200
+// }
+// console.log(plan2)
+
+// let plane3 = {
+//     name:"Ultimate",
+//     price:8.99,
+//     space:  100,
+//     transfer : 3000,
+//     pages: 30
+// }
+// plane3.pages = 25
+// console.log(plane3)
+
+// // Create an Object
+// const person = new Object();
+
+// // Add Properties
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue";
+
+// console.log(person)
+
+class toyotaCar{
+    start(){
+        console.log("black")
+    }
+    stop(){
+        console.log("white")
+    }
+}
+
+let fortuner = new toyotaCar()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
